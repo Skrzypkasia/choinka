770545bb8e80ffb3d0301ab1d9c55f609ddb98e6
Kasia <superkasia3@gmail.com>
2019-12-07 12:06:59 +0100

    repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for(int i=0; i<8; i++){
+	    for(int j=7; j>i; j--){
+            System.out.print(" ");
+        }
+	    for(int k=0; k<=i; k++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+	for(int i=2; i<8; i++){
+	    for(int j=7; j>i; j--){
+            System.out.print(" ");
+        }
+	    for(int k=0; k<=i; k++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+	for(int i=2; i<8; i++){
+	    for(int j=7; j>i; j--){
+            System.out.print(" ");
+        }
+	    for(int k=0; k<=i; k++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        }
+	for(int i= 0; i<3; i++){
+	    for(int j=1; j<=5; j++){
+            System.out.print(" ");
+        }
+	    for(int k=0; k<3; k++){
+            System.out.print("* ");
+        }
+        System.out.println();
+
+    }
+    }
+
+}
